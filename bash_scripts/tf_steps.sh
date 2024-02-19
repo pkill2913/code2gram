a6e6a4d86ebf01ac7cd8b5b275460a0505db0802
pkill2913 <oeco710@hotmail.com>
2024-02-19 17:21:23 -0500

    cluster added and fixing in bash

--- bash_scripts/tf_steps.sh
+++ bash_scripts/tf_steps.sh
@@ -2,8 +2,10 @@
 
 cd ./examples/ && terraform init
 
-cd ../examples/ && terraform plan -out=tfplan.binary
+terraform plan -out=tfplan.binary
+terraform show -json tfplan.binary > tfplan.json
+# cd ./examples/ && terraform plan -out=tfplan.binary
 
 # cd ../examples/ &&  docker run --rm -it hashicorp/terraform:latest plan -out=tfplan.binary
 
-cd ../examples/ && terraform show -json tfplan.binary > tfplan.json
+# cd ./examples/ && terraform show -json tfplan.binary > tfplan.json
--- main.py
+++ main.py
@@ -8,15 +8,17 @@ import json
 
 title = "EXAMPLE"
 vpc_id = "default"
-
+tf_json_file = "./examples/tfplan.json"
 
 def get_data_plan():
     with open('examples/tfplan.json', 'r') as file:
         plan_data = json.load(file)
     return plan_data
 
-def get_region(resources):
-    region = resources.get('configuration').get('provider_config').get('aws').get('expressions').get('region').get('constant_value')
+def get_region(tfstate_path):
+    with open(tfstate_path, 'r') as file:
+        tfstate_data = json.load(file)
+    region = tfstate_data.get('configuration').get('provider_config').get('aws').get('expressions').get('region').get('constant_value')
     return region
 
 def get_instances(resources):
@@ -39,20 +41,30 @@ def listar_recursos(tfstate_path):
 # resources = get_region(elementos)
 
 def generar_codigo_diagrama(elementos, archivo_salida):
+
+    indentacion = 4 
+
     with open(archivo_salida, 'w') as archivo:
         archivo.write("from diagrams import Diagram, Cluster\n")
         archivo.write("from diagrams.aws.compute import EC2\n\n")
         archivo.write("with Diagram(\"Diagrama de Elementos\", show=True):\n")
+
+        archivo.write(f"{(indentacion * ' ')}with Cluster(\"Region {region}\" ): \n")
+
+        if vpc_id == 'default':
+            archivo.write(f"{(indentacion + 4) * ' '}with Cluster(\"VPC Default\"):\n")
+        else:
+            archivo.write(f"{(indentacion + 4) * ' '}with Cluster(\"VPC tal y tal\"):\n")
         for elemento in elementos:
             print(elemento)
             print("##########")
             name = elemento['name']
             for clave, valor in elemento.items():
-                indentacion = 4  # Define el nivel de indentación deseado
+                 # Define el nivel de indentación deseado
                 if clave == 'type':
                     if valor == 'aws_instance':
-                        archivo.write(f"{indentacion * ' '}with Cluster(\"Cluster {title}\"):\n")
-                        archivo.write(f"{(indentacion + 4) * ' '}EC2(\"{name}\")\n")
+                        archivo.write(f"{(indentacion + 8) * ' '}with Cluster(\"Cluster {title}\"):\n")
+                        archivo.write(f"{(indentacion + 16) * ' '}EC2(\"{name}\")\n")
 
 # # Nombre del archivo de salida
 archivo_salida = "diagrama_generado.py"
@@ -61,8 +73,11 @@ print("Run")
 subprocess.run(["ls","-ls"])
 subprocess.run(["sh","-c","./bash_scripts/tf_steps.sh"])
 
+
+
 # # Generar el código del diagrama y guardar en el archivo
-elementos = listar_recursos('examples/tfplan.json')
+region = get_region(tf_json_file)
+elementos = listar_recursos(tf_json_file)
 generar_codigo_diagrama(elementos, archivo_salida)
 
 subprocess.run(["python", "diagrama_generado.py"])
